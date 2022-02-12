91d1e41e75e9bf2e53c3df51651fb2241054dd46
Patrick Mendonca <pat@mrmendonca.com>
2022-02-12 18:07:56 +0800

    works but slow?

--- day_15.py
+++ day_15.py
@@ -1,11 +1,14 @@
 import math
+import datetime
 # define positive infinity for distances in dijkstra
 import random
 
 positive_infinity = float('inf')
+swim_time = []
+sink_time = []
 
 # file i/o
-def get_data(file_name='test.dat'):
+def get_data(file_name='day_15.dat'):
     with open(file_name, 'r') as inf:
         raw_data = [line for line in inf.read().split('\n')]
     my_data = []
@@ -19,23 +22,29 @@ class PQ():
     def __init__(self, new_map=None):
         if not new_map:
             print('cant create empty queue')
-            return None
+            return
         else:
             self.queue = []
             self.list = []
             self.cost = []
             self.pos = []
-            self.inv = []
+            self.prev = []
+            self.neighbors = []
             self.max = len(new_map)
             for y in range(len(new_map)):
                 for x in range(len(new_map)):
                     self.list.append((x,y))
-                    self.cost.append(new_map[x][y])
+                    self.cost.append(new_map[y][x])
+                    self.neighbors.append(valid_edges((x, y), self.max))
+                    self.prev.append(-1)
 
     def insert(self, new_coord, new_cost):
-        print('adding', new_coord, new_cost, 'to PQ...')
+        # print('adding', new_coord, new_cost, 'to PQ...')
         self.queue.append([new_coord, new_cost])
+        global swim_time
+        swim_start = datetime.datetime.now()
         self.swim(len(self.queue)-1)
+        swim_time.append(datetime.datetime.now() - swim_start)
 
     def print(self):
         print('\nThis queue contains:')
@@ -46,50 +55,95 @@ class PQ():
 
     def poll(self):
         last_index = len(self.queue)-1
-        temp = self.queue[0]
-        self.queue[0] = self.queue[last_index]
-        self.queue[last_index] = temp
-        self.queue.pop(-1)
-        self.sink(0)
+        if last_index:
+            temp = self.queue[0]
+            self.queue[0] = self.queue[last_index]
+            self.queue[last_index] = temp
+            self.queue.pop(-1)
+            sink_start = datetime.datetime.now()
+            global sink_time
+            self.sink(0)
+            sink_time.append(datetime.datetime.now() - sink_start)
+        else:
+            self.queue.pop()
 
     def sink(self, node):
-        print('sinking node', node)
+        # print('sinking node', node, end='... ')
         if node == len(self.queue)-1:
-            print('at end, exiting')
+            # print('at end, exiting')
             return
-        lc = (2*node)+1
-        rc = (2*node)+2
-
-        if rc >= len(self.queue):
-            rc = None
-            less = lc
-        elif self.queue[lc][1] <= self.queue[rc][1]:
-            less = lc
-        else:
-            less = rc
         while True:
-            if self.queue[node][1] < self.queue[less][1]:
-                None
+            # print('sinking value', self.queue[node][1], end='... ')
+            ll = (4 * node) + 1
+            lc = (4 * node) + 2
+            rc = (4 * node) + 3
+            rr = (4 * node) + 4
+
+            if ll >= len(self.queue):
+                # print('no children!')
+                return
+            less = self.less(ll, lc, rc, rr)
+
+            # print('checking against node:', less, 'value:', self.queue[less][1], end='... ')
+            if self.queue[node][1] > self.queue[less][1]:
+                # print('sinking down...')
+                temp = self.queue[node]
+                self.queue[node] = self.queue[less]
+                self.queue[less] = temp
+                node = less
+                continue
+            # print('done sinking.')
             break
+
+    def less(self, a, b, c, d):
+        least = a
+        for x in a, b, c, d:
+            if x < len(self.queue):
+                if self.queue[x][1] < self.queue[least][1]:
+                    least = x
+            else:
+                return least
+        return least
 
 
     def swim(self, node):
-        print('swimming at node', node)
+        # print('swimming at node', node, end='... ')
         if node == 0:
-            print('at head, returning')
+            # print('at head, returning')
             return
         while True:
-            print('trying to swim with', self.queue[node][1])
-            parent_index = math.ceil((node/2)-1)
-            print('checking parent', self.queue[parent_index])
+            # print('trying to swim with', self.queue[node][1], end='... ')
+            parent_index = math.ceil((node/4)-1)
+            # print('checking parent', self.queue[parent_index], end='... ')
             if self.queue[node][1] < self.queue[parent_index][1]:
-                print('swimming upstream...')
+                # print('swimming upstream...')
                 temp = self.queue[node]
                 self.queue[node] = self.queue[parent_index]
                 self.queue[parent_index] = temp
                 node = parent_index
                 continue
+            # print('done swimming.')
             break
+
+    def show_route(self, target=None):
+        if not target:
+            prev = self.prev[-1]
+        else:
+            my_index = target[0] + (self.max * target[1])
+            prev = self.prev[my_index]
+        route = []
+        while prev != -1:
+            route.append(prev)
+            prev = self.prev[prev]
+
+        for i in range(len(self.cost)):
+            if i in route:
+                print(f'\x1b[6:30:42m{self.cost[i]}', end='')
+            else:
+                print(f'\x1b[0m{self.cost[i]}', end='')
+            if i % (self.max) == (self.max - 1):
+                print('\x1b[0m')
+        print()
 
 def valid_edges(my_coord, max):
     my_x = my_coord[0]
@@ -108,39 +162,114 @@ def valid_edges(my_coord, max):
 
 def dijk(my_map):
     global positive_infinity
-    print('entering dijkstra...', end='')
+    runtime = []
+    print('entering dijkstra...')
     vis = [False] * len(my_map)**2
-    prev = [None] * len(my_map)**2
     dist = [positive_infinity] * len(my_map)**2
+    count = 0
     dist[0] = 0
     my_q = PQ(my_map)
     my_q.insert((0,0), 0)
+    avg = datetime.datetime.now()
     while my_q.queue:
         ind = my_q.list.index(my_q.queue[0][0])
-        min_val = my_q.cost[ind]
+        min_val = dist[ind]
         vis[ind] = True
-        neighbors = valid_edges(my_q.list[ind], my_q.max)
-        for item in neighbors:
+        if count == 5000:
+            new = datetime.datetime.now()
+            avg = new - avg
+            count = 0
+            my_q.show_route(my_q.list[ind])
+            print('cost to this point:', dist[ind])
+            print('size of PQ:', len(my_q.queue))
+            print('time per 5k:', avg)
+            runtime.append([len(my_q.queue), avg])
+            avg = datetime.datetime.now()
+        count += 1
+        for item in my_q.neighbors[ind]:
             item_ind = my_q.list.index(item)
-            item_dist = dist[ind] + min_val
             if vis[item_ind] is True:
                 continue
+            item_dist = my_q.cost[item_ind] + min_val
             if item_dist < dist[item_ind]:
                 dist[item_ind] = item_dist
-                my_q.insert(my_q.list[item_ind], my_q.cost[item_ind])
+                my_q.prev[item_ind] = ind
+                my_q.insert(my_q.list[item_ind], item_dist)
+        if item_ind == len(my_map)**2-1:
+            print('I think I found the exit node.')
+            break
         my_q.poll()
 
-    my_q.print()
+    my_q.show_route()
+    print('exit cost:', dist[-1])
+    print('cost table:')
+    for line in runtime:
+        print(line)
 
 
+
+def grow_data(my_map):
+    print('growing data...')
+
+    factor = 2
+
+    # print(my_map)
+    new_map = []
+
+    row_len = len(my_map[0])
+
+    for row in my_map:
+        for i in range(factor):
+            temp = row[-row_len:]
+            for item in temp:
+                next = item+1
+                if next > 9:
+                    next = 1
+                row.append(next)
+
+    target = len(my_map)*factor
+    count = 0
+    while count < target:
+        new_row = []
+        for item in my_map[count]:
+            next = item + 1
+            if next > 9:
+                next = 1
+            new_row.append(next)
+        my_map.append(new_row)
+        count += 1
+
+    return my_map
 
 
 
 
 if __name__ == "__main__":
 
+    start_time = datetime.datetime.now()
+
+    print ('starting at', start_time)
     data = get_data()
+
+    # if input('Should I expand the map? y/n?') is 'y':
+    data = grow_data(data)
+
     dijk(data)
+
+    end_time = datetime.datetime.now()
+    elapsed_time = end_time - start_time
+
+    print('finished at', end_time)
+    print('total seconds elapsed:', elapsed_time)
+
+
+    with open('sink.dat', 'w') as outf:
+        for item in sink_time:
+            outf.write(f'{item},')
+
+    with open('swim.dat', 'w') as outf:
+        for item in swim_time:
+            outf.write(f'{item},')
 
 
     print('\n\nExiting...')
