71ab24d4f54ea956d768963691564abba2e2526e
Patrick Mendonca <pat@mrmendonca.com>
2022-02-13 16:01:13 +0800

    Day 16 basic packets working

--- day_16.py
+++ day_16.py
@@ -60,12 +60,17 @@ def filter_signal(signal, my_ending_bit):
     return my_ending_bit
 
 
-
 if __name__ == '__main__':
     signal = read_file()
     print(signal)
     signal = process_signal(signal)
     print(signal)
+    i=0
+    for char in signal:
+        i += 1
+        print(char, end='')
+        if i % 3 == 0:
+            print()
     processed_signal = []
     while signal:
         ending_bit = 6
@@ -77,35 +82,67 @@ if __name__ == '__main__':
             literal = bin_to_dec(literal)
             processed_signal.append([version, packet_type, literal])
         else:
-            length_id = signal[ending_bit]
-            ending_bit += 1
-            if length_id is '0':
-                sub_packet_length = bin_to_dec(signal[ending_bit:ending_bit+15])
-                ending_bit += 15
-                target = ending_bit + sub_packet_length
-                processed_signal.append([version, packet_type, f'length: {sub_packet_length} bits'])
-                while ending_bit != target:
-                    sub_packet_version = bin_to_dec(signal[ending_bit:ending_bit+3])
-                    sub_packet_type = bin_to_dec(signal[ending_bit+3:ending_bit+6])
-                    ending_bit += 6
-                    print(f'Sub packet found: V.{sub_packet_version} T.{sub_packet_type}')
-                    literal, ending_bit = type_4(signal, ending_bit)
-                    literal = bin_to_dec(literal)
-                    processed_signal.append([sub_packet_version, sub_packet_type, literal])
-            else:
-                sub_packet_length = bin_to_dec(signal[ending_bit:ending_bit+11])
-                ending_bit += 11
-                processed_signal.append([version, packet_type, f'length: {sub_packet_length} packets'])
-                for i in range(sub_packet_length):
-                    sub_packet_version = bin_to_dec(signal[ending_bit:ending_bit+3])
-                    sub_packet_type = bin_to_dec(signal[ending_bit+3:ending_bit+6])
-                    ending_bit += 6
-                    print(f'Sub packet found: V.{sub_packet_version} T.{sub_packet_type}')
-                    literal, ending_bit = type_4(signal, ending_bit)
-                    literal = bin_to_dec(literal)
-                    processed_signal.append([sub_packet_version, sub_packet_type, literal])
+            done = False
+            sub_count = [0]
+            while not done:
+                if packet_type != 4:
+                    length_id = signal[ending_bit]
+                    ending_bit += 1
+                if length_id is '0':
+                    sub_packet_length = bin_to_dec(signal[ending_bit:ending_bit+15])
+                    # sub_count.append(sub_packet_length)
+                    ending_bit += 15
+                    target = ending_bit + sub_packet_length
+                    processed_signal.append([version, packet_type, f'length: {sub_packet_length} bits'])
+                    while ending_bit != target:
+                        sub_packet_version = bin_to_dec(signal[ending_bit:ending_bit+3])
+                        sub_packet_type = bin_to_dec(signal[ending_bit+3:ending_bit+6])
+                        ending_bit += 6
+                        print(f'Sub packet found: V.{sub_packet_version} T.{sub_packet_type}')
+                        if sub_packet_type != 4:
+                            version = sub_packet_version
+                            packet_type = sub_packet_type
+                            sub_count[-1] -= 1
+                            break
+                        literal, ending_bit = type_4(signal, ending_bit)
+                        literal = bin_to_dec(literal)
+                        processed_signal.append([sub_packet_version, sub_packet_type, literal])
+                        # do we need to adjust ending bit if we're sub packet?
+                        # ending_bit = filter_signal(signal, ending_bit)
+                        continue
+                    done = True
+                else:
+                    sub_packet_length = bin_to_dec(signal[ending_bit:ending_bit+11])
+                    sub_count.append(sub_packet_length)
+                    ending_bit += 11
+                    processed_signal.append([version, packet_type, f'length: {sub_packet_length} packets'])
+                    for i in range(sub_packet_length):
+                        sub_packet_version = bin_to_dec(signal[ending_bit:ending_bit+3])
+                        sub_packet_type = bin_to_dec(signal[ending_bit+3:ending_bit+6])
+                        ending_bit += 6
+                        print(f'Sub packet found: V.{sub_packet_version} T.{sub_packet_type}')
+                        if sub_packet_type != 4:
+                            version = sub_packet_version
+                            packet_type = sub_packet_type
+                            break
+                        literal, ending_bit = type_4(signal, ending_bit)
+                        literal = bin_to_dec(literal)
+                        processed_signal.append([sub_packet_version, sub_packet_type, literal])
+                        sub_count[-1] -= 1
+                        # don't adjust ending bit for sub-packets
+                        # ending_bit = filter_signal(signal, ending_bit)
+                        if not sub_count[-1]:
+                            sub_count.pop(-1)
+                            done = True
+                    # i think we need adjustment here,
+                    # but only if we've used type 4 and are "done"
+                    if done:
+                        ending_bit = filter_signal(signal, ending_bit)
 
-        ending_bit = filter_signal(signal, ending_bit)
+
         signal = signal[ending_bit:]
 
-
+    sum = 0;
+    for item in processed_signal:
+        sum += item[0]
+    print('Packet Version Sum:', sum)
