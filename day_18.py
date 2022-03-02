e195338cbc4357132cbd4c216c994b92b41d7779
Patrick Mendonca <pat@mrmendonca.com>
2022-03-02 18:31:02 +0800

    day_18 part 1 start

--- day_18.py
+++ day_18.py
@@ -23,10 +23,12 @@ def solve(my_homework):
     # we are only ever working on the first line (index 0)
     while my_homework:
         # for the line we are looking at start by reducing across left to right
+        print(f'Evaluating {my_homework[0]}...')
         my_homework[0] = reduced(my_homework[0])
         # once a line is reduced, check to see if there is another line below
         # if there is a line below append it to the first line and then remove it
         if len(my_homework) > 1:
+            print(f'\x1b[0:30:44mFound new line, adding {my_homework[0]} + {my_homework[1]}\x1b[0m')
             temp_string = ''
             temp_string += '['
             temp_string += my_homework[0]
@@ -40,7 +42,31 @@ def solve(my_homework):
         # if there were no more lines, exit the loop
         else:
             break
-    print(my_homework)
+    print(my_homework[0])
+
+
+def untangle(my_string):
+    """
+    untangle() takes a string and returns a list of char and int
+    :param my_string: 'str'
+        the string, consists of [,] and 0-9 char
+    :return:
+        a list of individual items, either char (if [,]) or int (if a numeral)
+    """
+    untangled_string_list = []
+    while my_string:
+        temp_string = my_string[0]
+        my_string = my_string[1:]
+        if '0' <= temp_string <= '9':
+            while '0' <= my_string[0] <= '9':
+                temp_string += my_string[0]
+                my_string = my_string[1:]
+            temp_string = int(temp_string)
+
+        untangled_string_list.append(temp_string)
+
+    return untangled_string_list
+
 
 
 def reduced(my_string):
@@ -55,31 +81,61 @@ def reduced(my_string):
     reduced_string = ''
 
     # split the string to individual characters so we can read left ot right easier
-    string_list = [i for i in list(my_string)]
+    string_list = untangle(my_string)
 
     # start a while true loop so we can start back at index 0 while reducing
     # we do this so we can reduce more than one item in our string
     while True:
+        print('Trying to reduce:')
+        error_check = 0
+        error_list = []
+        for item in string_list:
+            if item is '[':
+                error_check += 1
+                error_list.append(error_check)
+            elif item is ']':
+                error_list.append(error_check)
+                error_check -= 1
+            else:
+                error_list.append(' ')
+            print(item, end='')
+        print()
+        for item in error_list:
+            print(item, end='')
+        print()
         # flag for exit while loop
         reduced_flag = False
         # variable to track how deep in sub-numbers we are, this is for checking
         # if we need to explode a number that is four levels deep
         level = 0
         # start looking character by character in the list
-        for char in string_list:
-            if char is '[':
+        for i in range(len(string_list)):
+            if string_list[i] is '[':
                 level += 1
-            elif char is ']':
+            elif string_list[i] is ']':
                 level -= 1
 
             # if we are at 5 opening brackets we are in level 4, and need to explode
             if level == 5:
-
-                explode(string_list)
+                print(f'Found item to explode...')
+                if type(string_list[i+1]) is int and string_list[i+1] >= 10:
+                    print('Found out the following item needs split')
+                    string_list = go_split(string_list, i+1)
+                    reduced_flag = True
+                    break
+                string_list = explode(string_list, i)
                 # set the reduced flag to repeat while(True) loop from the top
                 reduced_flag = True
                 # break the for char loop and repeat from the top
                 break
+
+
+            if type(string_list[i]) is int and string_list[i] >= 10:
+                print('Found item to split')
+                string_list = go_split(string_list, i)
+                reduced_flag = True
+                break
+
 
 
         # if we've moved across the string and haven't reduced at all,
@@ -89,9 +145,96 @@ def reduced(my_string):
 
     # copy the adjusted string_list to a single string variable for return
     for i in string_list:
-        reduced_string += i
+        try:
+            reduced_string += i
+        except TypeError:
+            reduced_string += str(i)
+
+    print(f'\x1b[0;30;41mFinal reduction is {reduced_string}\x1b[0m')
 
     return reduced_string
+
+
+def go_split(string_list, index):
+    """
+    go_split() performs the split reduction on the numbers
+    :param string_list: [char, char/int, char/int... ]
+        the list of chars that make up our number
+    :param index: int
+        the index of the char in the list to split
+    :return:
+        list of strings after reduction
+    """
+    print(string_list[index], '->', end='')
+    # split the int value to two ints, rounding down on the first item
+    left_value = int(string_list[index] / 2)
+    right_value = string_list[index] - left_value
+
+    # break the list into two so we can insert new items
+    left_list = string_list[:index]
+    right_list = string_list[index + 1:]
+    insert_list = ['[', left_value, ',', right_value, ']']
+    print(insert_list)
+
+    string_list = left_list
+    for i in insert_list:
+        string_list.append(i)
+    for i in right_list:
+        string_list.append(i)
+
+    return string_list
+
+
+def explode(string_list, index):
+    """
+    explode() performs the explode reduction on the numbers
+    :param string_list: [char, char/int, char/int... ]
+        the list of chars that make up our number
+    :param index: int
+        the index of the char in the list to begin explode operation
+    :return:
+        list of strings after reduction
+    """
+    print(f'Exploding at index {index}...')
+    while string_list[index+1] is '[':
+        print('Oops, found sub-item, moving to split that one!')
+        index += 1
+    print(f'I think I am looking at {string_list[index:index+5]}')
+    end_pair_index = index + 4
+    # get the initial values in our pair for later addition
+    left_int = string_list[index+1]
+    right_int = string_list[index+3]
+
+    # find the index of the left and right numeral, if they exist
+    explode_left, explode_right = None, None
+    # start at left bracket, look left (-1 index) for the neighbor int
+    i = index
+    while i > 0:
+        i -= 1
+        if type(string_list[i]) is int:
+            print(f'found {string_list[i]} to the left...')
+            explode_left = i
+            break
+    # start at right bracket, look right (+1 index) for the neighbor int
+    i = end_pair_index
+    while i < len(string_list)-1:
+        i += 1
+        if type(string_list[i]) is int:
+            print(f'found {string_list[i]} to the right...')
+            explode_right = i
+            break
+
+    # we have left and right indexes now. If there was no int left or right, that value is (None)
+    if explode_left is not None:
+        string_list[explode_left] = string_list[explode_left] + left_int
+    if explode_right is not None:
+        string_list[explode_right] = string_list[explode_right] + right_int
+
+    # replace the entire bracketed substring with a 0
+    string_list[index] = 0
+    del string_list[index + 1: end_pair_index + 1]
+
+    return string_list
 
 if __name__ == '__main__':
 
